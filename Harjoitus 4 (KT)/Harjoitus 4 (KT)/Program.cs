23874c5864cc2e8d3944fab3ae53f6b7ef7ad966
3dw1z4rd <edwardsalmivaara1@gmail.com>
2022-11-17 10:11:05 +0200

    Taso-4
    Edin tekemä taso nelonen.

---
+++ Harjoitus 4 (KT)/Harjoitus 4 (KT)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_4__KT_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Albumi albumi1 = new Albumi();
+
+
+            albumi1.Nimi = "Thriller";
+            albumi1.Artisti = "Michael Jackson";
+            albumi1.Genre = "Pop, post-disco, funk, rock";
+            albumi1.Hinta = "100$";
+            albumi1.TulostaKappaleet();
+            albumi1.TulostaData();
+            string albuminTiedot = albumi1.ToString();
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
