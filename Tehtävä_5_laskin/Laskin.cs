af2af40aa33ce4324aae38b2c7f6023950375109
3dw1z4rd <114394526+3dw1z4rd@users.noreply.github.com>
2022-12-07 20:18:33 +0200

    Tehtävä 5 Laskin

---
+++ Tehtävä_5_laskin/Laskin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tehtävä_5_laskin
+{
+    static class Laskin
+    {
+        public static float Summa(float a, float b) // Tehdään jokaisille oliolle  oma "Logiikka".
+        {
+            return a + b;
+        }
+        public static float Erotus(float a, float b)
+        {
+            return a - b;
+        }
+        public static float Kertolasku(float a, float b)
+        {
+            return a * b;
+        }
+        public static float Jakolasku(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tehtävä_5_laskin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tehtävä_5_laskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 5.0f;  //Määritellään arvot.
+            float b = 2.5f;
+
+            float summa = Laskin.Summa(a, b);
+            float erotus = Laskin.Erotus(a, b);
+            float kertolasku = Laskin.Kertolasku(a, b);
+            float jakolasku = Laskin.Jakolasku(a, b);
+            Console.WriteLine("Numeroiden {0} ja {1}:", a, b);
+            Console.WriteLine("summa: " + summa);
+            Console.WriteLine("Erotus: " + erotus);
+            Console.WriteLine("Kertolasku: " + kertolasku);
+            Console.WriteLine("Jakolasku: " + jakolasku);
+            Console.ReadLine();
+        }
+    }
+}
