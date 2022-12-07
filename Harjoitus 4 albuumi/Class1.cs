74ccfc2d94a6aee45d34f737641f50720679b327
3dw1z4rd <114394526+3dw1z4rd@users.noreply.github.com>
2022-12-07 20:08:25 +0200

    Harjoitus 4
    Harjoitus 4 albuumi palautettu.

---
+++ Harjoitus 4 albuumi/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tehtava4albumi
+{
+    class Albumi
+
+    {
+
+        public string Artisti { get; set; }
+        public string Nimi { get; set; }
+        public string Genre { get; set; }
+        public string Hinta { get; set; }
+        public string Kappaleet { get; set; }
+
+
+
+
+
+
+        List<string> PriceList = new List<string>(5);
+
+        public void TulostaKappaleet()
+        {
+
+            PriceList.Add("---Onko Kirkkonummi hereillä pt 2");
+            PriceList.Add("---Mc Rambo on täällä");
+            PriceList.Add("---Me tullaan kyliltä");
+            PriceList.Add("---Kirkkonummi joka päivä pt 1");
+            PriceList.Add("---Kirkkonummi joka päivä pt 2");
+        }
+
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Albumi: ");
+            Console.WriteLine("--- Artisti:" + Artisti);
+            Console.WriteLine("--- Genre: " + Genre);
+            Console.WriteLine("--- Hinta: " + Hinta);
+            Console.WriteLine("--- Nimi: " + Nimi);
+            Console.WriteLine("--- Kappaleet: " + Kappaleet);
+            PriceList.Sort();
+            PriceList.ForEach(Console.WriteLine);
+
+
+        }
+
+
+    }
+}
+
---
+++ Harjoitus 4 albuumi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tehtava4albumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Albumi albumi1 = new Albumi();
+            {
+
+                albumi1.Nimi = "Kirkkonummi joka päivä";
+                albumi1.Artisti = "Mc Rambo";
+                albumi1.Genre = "Rap, Finnish rap";
+                albumi1.Hinta = "10000$";
+                albumi1.TulostaKappaleet();
+                albumi1.TulostaData();
+                string albuminTiedot = albumi1.ToString();
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
+
