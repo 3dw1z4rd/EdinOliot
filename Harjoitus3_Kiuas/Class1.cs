a2d253ed90b4fac4ed5e669745af0a6e489c93f6
3dw1z4rd <114394526+3dw1z4rd@users.noreply.github.com>
2022-12-07 20:13:44 +0200

    Harjoitus 3 Kiuas

---
+++ Harjoitus3_Kiuas/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Harjoitus3_Kiuas;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Kiuas kiuas1 = new Kiuas("Harvia", 0);
+
+            // kiuas1.TulostaData();
+            kiuas1.onOFF();
+
+            kiuas1.MuokkaaLämpötilaa(80);
+            kiuas1.TulostaData();
+
+
+            Kiuas kosteus1 = new Kiuas("Harvia", 0);
+
+            //kosteus1.TulostaData();
+
+            kosteus1.MuokkaaLämpötilaa(5);
+            kosteus1.TulostaData();
+            kiuas1.onOFF();
+
+        }
+    }
+}
